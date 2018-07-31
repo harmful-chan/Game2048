330ce7605cc0e3e3f307588416502f5be0790854
harmful-chan <1148706823@qq.com>
2018-07-31 16:35:03 +0000

    修改源码，增加了指针转换

--- Game2048.cpp
+++ Game2048.cpp
@@ -1,4 +1,5 @@
 #include "Game2048.h"
+#include <string.h>
 #include <stdlib.h>
 namespace ge{
 
@@ -6,17 +7,18 @@ namespace ge{
 Game2048::Game2048()
 { 
     pPanel = (unit_t *)malloc(4*4*sizeof(unit_t));
-    memset(pPanel, 0, sizeof(pPanel));
+    //pPanel = new unit_t[4][4];
+    memset(pPanel, 0, 4*4*sizeof(unit_t));
 }
 
 //s释放面板空间
 Game2048::~Game2048()
 {
-    delete nPanel;
+    delete pPanel;
 }
 
 //按方向刷新屏幕
-Game2048::RefreshPanel(Dir tAction)
+void Game2048::RefreshPanel(Dir tAction)
 {
     MergeNumber(tAction);
     MoveNumber(tAction);
@@ -24,92 +26,43 @@ Game2048::RefreshPanel(Dir tAction)
 }
 
 //合并数字
-Game2048::MergeNumber(Dir tAction)
+void Game2048::MergeNumber(Dir tAction)
 {
     int x = 0;
     int y = 0;
     int stat = 1;
+
+    unit_t (*p)[4] = (unit_t (*)[4])pPanel;    //一维数组转二维数组指针
+
     for(int i = 0; i < 4; i++)
     {
         for(int j = 1; j < 4; j++)
         {
-            while(stat)
+      
             switch(stat)
             {
                 case 1:
-                         if(tAction == LETF)   stat = 2;
+                         if(tAction == LEFT)   stat = 2;
                     else if(tAction == UPWARD) stat = 3;
                     else if(tAction == RIGHT)  stat = 4;
                     else if(tAction == DOWN)   stat = 5;
                 break;
-                if(ZERO != *(nPanel[j][i])) 
-                    MergeIdentical(*(nPanel[0][i]), *(nPanel[j][i]));
+                if(ZERO != p[j][i]) 
+                    MergeIdentical(&(p[0][i]), &(p[j][i]));
             
-                if(ZERO != *(nPanel[j][i])) 
-                    MergeIdentical(*(nPanel[0][i]), *(nPanel[j][i]));
+                if(ZERO != p[j][i]) 
+                    MergeIdentical(&(p[0][i]), &(p[j][i]));
          
-                if(ZERO != *(nPanel[i][3-j])) 
-                    MergeIdentical(*(nPanel[i][3]), *(nPanel[i][3-j]));
+                if(ZERO != p[i][3-j]) 
+                    MergeIdentical(&(p[i][3]), &(p[i][3-j]));
       
-                if(ZERO != *(nPanel[3-j][i])) 
-                    MergeIdentical(*(nPanel[3][i]), *(nPanel[3-j][i]));
+                if(ZERO != p[3-j][i]) 
+                    MergeIdentical(&(p[3][i]), &(p[3-j][i]));
             }
         }
     }    
-
-    //向左
-    for(int i = 0; i < 4; i++)
-    {
-        for(int j = 1; j < 4; j++)
-        {
-            if(ZERO != *(nPanel[i][j])) 
-            {
-                MergeIdentical(*(nPanel[i][0]), *(nPanel[i][j]));
-                break;
-            }
-        }
-    }
-    //向上
-    for(int i = 0; i < 4; i++)
-    {
-        for(int j = 1; j < 4; j++)
-        {
-            if(ZERO != *(nPanel[j][i])) 
-            {
-                MergeIdentical(*(nPanel[0][i]), *(nPanel[j][i]));
-                break;
-            }
-        }
-    } 
-
-    //向右
-    for(int i = 0; i < 4; i++)
-    {
-        for(int j = 1; j < 4; j++)
-        {
-            if(ZERO != *(nPanel[i][3-j])) 
-            {
-                MergeIdentical(*(nPanel[i][3]), *(nPanel[i][3-j]));
-                break;
-            }
-        }
-    } 
-
-    //向下
-    for(int i = 0; i < 4; i++)
-    {
-        for(int j = 1; j < 4; j++)
-        {
-            if(ZERO != *(nPanel[3-j][i])) 
-            {
-                MergeIdentical(*(nPanel[3][i]), *(nPanel[3-j][i]));
-                break;
-            }
-        }
-    }
 }
 
-
-
-
 };
+
+
