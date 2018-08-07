63b55e611304e5d4bf9bd45fdee80f5f4c9f3512
harmful-chan <1148706823@qq.com>
2018-08-07 06:09:28 +0000

    添加是同注释

--- Game2048.h
+++ Game2048.h
@@ -10,6 +10,8 @@ namespace ge
 
 typedef unsigned int unit_t; 
 
+
+/* 方向定义 */
 typedef enum FreshDirection
 {
     UP = 1,
@@ -23,24 +25,22 @@ class Game2048
 private :
     unit_t *pPanel;
 
-    //int MergeNumber(Dir tAction);    //合并数字
-    //int MoveNumber(Dir tAction);    //数字移动
-    //void AddNumber();    //空白处添加数字
+    int MergeNumber(Dir tAction);    //合并数字,执行完成返回0, 否则-1
+    int MoveNumber(Dir tAction);    //数字移动,执行完成返回0,否则-1
+    void AddNumber();    //空白区域产生随机数
 
     int MergeIdentical(unit_t *pSrcNum, unit_t *pDetNum);    //相同数字合并
-    int ExchangeIdentical(unit_t *pSrcNum, unit_t *pDetNum);    //交换数字    
+    int ExchangeIdentical(unit_t *pSrcNum, unit_t *pDetNum);    //交换数字 
+
+
 public :
-    Game2048();    //面板初始化
-    Game2048(unit_t *p){pPanel = p;};
+    Game2048();    //面板初始化空间为空
+    Game2048(unit_t *p){pPanel = p;};    //人为初始化
     ~Game2048();       
     void RefreshPanel(Dir tAction);    //面板刷新方向
 
-    //测试区
-    void AddNumber();
-    void Show();
-
-    int MergeNumber(Dir tAction);    //合并数字
-    int MoveNumber(Dir tAction);    //数字移动
+    //测试
+    void Show();    //显示
     
 };
 
